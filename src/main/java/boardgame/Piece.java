5dfcc15be5d5d4ab1bd2306e15b45b5f36c1a4f5
angelavas <angelavasconcelos780@gmail.com>
2023-02-15 17:57:10 -0300

    Chess layer and printing the board

--- src/main/java/boardgame/Piece.java
+++ src/main/java/boardgame/Piece.java
@@ -1,2 +1,15 @@
-package boardgame;public class Piece {
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    protected Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+    protected Board getBoard() {
+        return board;
+    }
+
 }
